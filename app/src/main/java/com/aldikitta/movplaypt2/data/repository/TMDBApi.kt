9c4a5400ae64f8b9148ab471d79edfe6c12b2ce4
Aldikitta <andi.kitta77@gmail.com>
2022-06-30 20:35:51 +0800

    getUpcomingMovies fun

--- app/src/main/java/com/aldikitta/movplaypt2/data/repository/TMDBApi.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/data/repository/TMDBApi.kt
@@ -2,6 +2,7 @@ package com.aldikitta.movplaypt2.data.repository
 
 import com.aldikitta.movplaypt2.BuildConfig.API_KEY
 import com.aldikitta.movplaypt2.data.repository.responses.MovieResponse
+import com.aldikitta.movplaypt2.util.Constants.LANGUAGE_EN
 import com.aldikitta.movplaypt2.util.Constants.STARTING_PAGE_INDEX
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,13 +12,20 @@ interface TMDBApi {
     suspend fun getTrendingTodayMovies(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
-        @Query("language") language: String = "en"
+        @Query("language") language: String = LANGUAGE_EN
     ): MovieResponse
 
     @GET("trending/popular")
     suspend fun getPopularMovies(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
-        @Query("language") language: String ="en"
+        @Query("language") language: String = LANGUAGE_EN
+    )
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("page") page: Int = STARTING_PAGE_INDEX,
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("language") language: String = LANGUAGE_EN
     )
 }
--- app/src/main/java/com/aldikitta/movplaypt2/util/Constants.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/util/Constants.kt
@@ -3,6 +3,7 @@ package com.aldikitta.movplaypt2.util
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val STARTING_PAGE_INDEX = 0
+    const val LANGUAGE_EN = "en"
     const val IMAGE_BASE_UR = "https://image.tmdb.org/t/p/w500/"
     const val DATABASE_NAME = "favorites_database"
     const val TABLE_NAME = "favorites_table"
