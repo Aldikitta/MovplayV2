a6de8c82c5577eb03312851a610315d8b4e1504d
Aldikitta <andi.kitta77@gmail.com>
2022-06-30 21:28:44 +0800

    Movie and TvShow response

--- app/src/main/java/com/aldikitta/movplaypt2/data/repository/MovieTMDBApi.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/data/repository/MovieTMDBApi.kt
@@ -20,26 +20,26 @@ interface MovieTMDBApi {
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): MovieResponse
 
     @GET("movie/upcoming")
     suspend fun getUpcomingMovies(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): MovieResponse
 
     @GET("movie/top_rated")
     suspend fun getTopRatedMovies(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): MovieResponse
 
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovies(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): MovieResponse
 }
--- app/src/main/java/com/aldikitta/movplaypt2/data/repository/TvShowTMDBApi.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/data/repository/TvShowTMDBApi.kt
@@ -1,6 +1,7 @@
 package com.aldikitta.movplaypt2.data.repository
 
 import com.aldikitta.movplaypt2.BuildConfig.API_KEY
+import com.aldikitta.movplaypt2.data.repository.responses.tvshowresponses.TVShowResponse
 import com.aldikitta.movplaypt2.util.Constants.LANGUAGE_EN
 import com.aldikitta.movplaypt2.util.Constants.STARTING_PAGE_INDEX
 import retrofit2.http.GET
@@ -12,33 +13,33 @@ interface TvShowTMDBApi {
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): TVShowResponse
 
     @GET("trending/tv/day")
     suspend fun getTrendingTvShows(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): TVShowResponse
 
     @GET("tv/on_the_air")
     suspend fun getOnTheAirTvShows(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): TVShowResponse
 
     @GET("tv/popular")
     suspend fun getPopularTvShows(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): TVShowResponse
 
     @GET("tv/airing_today")
     suspend fun getAiringTodayTvShows(
         @Query("page") page: Int = STARTING_PAGE_INDEX,
         @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE_EN
-    )
+    ): TVShowResponse
 }
