e6a0388055aae5ea7a1dfdc65a9473bff1e4fefc
Aldikitta <andi.kitta77@gmail.com>
2022-07-01 21:27:51 +0800

    BottomNavItem

--- app/src/main/java/com/aldikitta/movplaypt2/model/BottomNavItem.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/model/BottomNavItem.kt
@@ -1,17 +1,36 @@
-//package com.aldikitta.movplaypt2.model
-//
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.Home
-//import androidx.compose.material3.Icon
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.graphics.vector.ImageVector
-//
-//sealed class BottomNavItem(val title: String, val icon: @Composable () -> Unit, val destination: Destination) {
-//    object Home : BottomNavItem(
-//        HomeScreenDestination, {
-//            Icon(imageVector = Icons.Filled.Home, contentDescription = "Home")
-//        }, title = "Home"
-////        icon = Icon(imageVector = , contentDescription = ),
-////        destination = HomeScreenDestination
-//    )
-//}
+package com.aldikitta.movplaypt2.model
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import com.aldikitta.movplaypt2.screens.destinations.AccountScreenDestination
+import com.aldikitta.movplaypt2.screens.destinations.Destination
+import com.aldikitta.movplaypt2.screens.destinations.FavoritesScreenDestination
+import com.aldikitta.movplaypt2.screens.destinations.HomeScreenDestination
+
+sealed class BottomNavItem(
+    val title: String,
+    val icon: @Composable () -> Unit,
+    val destination: Destination
+) {
+    object Home : BottomNavItem(
+        "Home", {
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Home")
+        }, HomeScreenDestination
+    )
+
+    object Favorites : BottomNavItem(
+        "Favorites", {
+            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favorites")
+        }, FavoritesScreenDestination
+    )
+
+    object Account : BottomNavItem(
+        "Account", {
+            Icon(imageVector = Icons.Filled.Person, contentDescription = "Account")
+        }, AccountScreenDestination
+    )
+}
