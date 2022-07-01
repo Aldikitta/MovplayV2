b4a96c70304dbd135f9f93b88b646a0aa2316e9b
Aldikitta <andi.kitta77@gmail.com>
2022-07-01 21:21:17 +0800

    Account Screen and Favorite Screen

---
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/account/AccountScreen.kt
@@ -0,0 +1,9 @@
+package com.aldikitta.movplaypt2.screens.account
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun AccountScreen() {
+}
