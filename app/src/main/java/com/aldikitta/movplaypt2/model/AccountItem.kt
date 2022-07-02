f2375455a3ea10631a1596c66508e28f79d7d61f
Aldikitta <andi.kitta77@gmail.com>
2022-07-02 20:32:43 +0800

    AccountScreen part 1

--- app/src/main/java/com/aldikitta/movplaypt2/model/AccountItem.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/model/AccountItem.kt
@@ -1,6 +1,9 @@
 package com.aldikitta.movplaypt2.model
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class AccountItem(
     val title: String,
-    val icon: Int
+    val icon: ImageVector
 )
--- app/src/main/java/com/aldikitta/movplaypt2/screens/account/AccountScreen.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/account/AccountScreen.kt
@@ -1,15 +1,67 @@
 package com.aldikitta.movplaypt2.screens.account
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.RateReview
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import com.aldikitta.movplaypt2.screens.account.about.AboutScreen
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.aldikitta.movplaypt2.R
+import com.aldikitta.movplaypt2.model.AccountItem
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun AccountScreen(
-    navigator: DestinationsNavigator
-
+    navigator: DestinationsNavigator,
+    navController: NavController
 ) {
-    AboutScreen(navigator = navigator)
+    Scaffold {innerPadding ->
+        Image(
+            painter = painterResource(id = R.drawable.movcolored),
+            contentDescription = null
+        )
+        LazyColumn(
+            contentPadding = innerPadding,
+            content = {
+                val accountItems = listOf(
+                    AccountItem(
+                        "About",
+                        Icons.Filled.Info
+                    ),
+                    AccountItem(
+                        "Rate Us",
+                        Icons.Filled.RateReview
+                    ),
+                    AccountItem(
+                        "Share",
+                        Icons.Filled.Share
+                    ),
+                    AccountItem(
+                        "Stay connected",
+                        Icons.Filled.Chat
+                    ),
+                )
+                items(accountItems){item ->
+                    AccountItems(
+                        accountItem = item
+                    )
+                }
+            }
+        )
+    }
 }
+
+@Composable
+fun AccountItems(
+    accountItem: AccountItem,
+){}
