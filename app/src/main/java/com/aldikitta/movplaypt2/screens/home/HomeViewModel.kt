b1569481752ef41707ccf5044f8a64db6fbf159b
Aldikitta <andi.kitta77@gmail.com>
2022-07-03 20:20:05 +0800

    HomeViewModel, initialize constructor

--- app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeViewModel.kt
@@ -1,4 +1,25 @@
 package com.aldikitta.movplaypt2.screens.home
 
-class HomeViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.aldikitta.movplaypt2.data.repository.movie.MoviesGenresRepository
+import com.aldikitta.movplaypt2.data.repository.movie.MoviesRepository
+import com.aldikitta.movplaypt2.data.repository.tvshow.TvShowsGenresRepository
+import com.aldikitta.movplaypt2.data.repository.tvshow.TvShowsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val moviesRepository: MoviesRepository,
+    private val moviesGenresRepository: MoviesGenresRepository,
+    private val tvShowsRepository: TvShowsRepository,
+    private val tvShowsGenresRepository: TvShowsGenresRepository
+) : ViewModel() {
+    private val _selectedOption = mutableStateOf("Movies")
+    val selectedOption: State<String> = _selectedOption
+
+    private val _selectedGenre = mutableStateOf("")
+    val selectedGenre: State<String> = _selectedGenre
 }
