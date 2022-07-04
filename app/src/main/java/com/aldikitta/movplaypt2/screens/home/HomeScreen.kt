c64907f1087cb4a99e0c146503a0d63256b86595
Aldikitta <andi.kitta77@gmail.com>
2022-07-04 16:03:18 +0800

    Home Screen part 1

--- app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeScreen.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeScreen.kt
@@ -1,13 +1,22 @@
 package com.aldikitta.movplaypt2.screens.home
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @RootNavGraph(start = true)
 @Destination
 @Composable
-fun HomeScreen() {
-Text(text = "Hello")
+fun HomeScreen(
+    navigator: DestinationsNavigator,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    val trendingMovies = viewModel.trendingMovies.value.collectAsLazyPagingItems()
+    val upComingMovies = viewModel.upcomingMovies.value.collectAsLazyPagingItems()
+    val topRatedMovies = viewModel.topRatedMovies.value.collectAsLazyPagingItems()
+    val nowPlayingMovies = viewModel.nowPlayingMovies.value.collectAsLazyPagingItems()
+    val popularMovies = viewModel.popularMovies.value.collectAsLazyPagingItems()
 }
