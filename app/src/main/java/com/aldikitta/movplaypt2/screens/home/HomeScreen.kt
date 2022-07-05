b70bd75c54858fffdd6201296cf6b68311393a28
Aldikitta <andi.kitta77@gmail.com>
2022-07-05 15:13:26 +0800

    Home Screen part 5 (genre chips)

--- app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeScreen.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/HomeScreen.kt
@@ -3,19 +3,21 @@ package com.aldikitta.movplaypt2.screens.home
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -26,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.aldikitta.movplaypt2.R
+import com.aldikitta.movplaypt2.model.Genre
 import com.aldikitta.movplaypt2.screens.commons.MovplayToolbar
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
@@ -78,13 +81,26 @@ fun HomeScreen(
             }
         )
 
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = 8.dp)
+        ) {
             item {
                 Category(
                     items = listOf("Movies", "Tv Shows"),
                     viewModel = viewModel,
                     modifier = Modifier.fillMaxWidth()
                 )
+                Spacer(modifier = Modifier.height(5.dp))
+            }
+            item {
+                Text(
+                    text = "Genres",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            item {
+                GenreOptionV2(viewModel = viewModel)
             }
         }
     }
@@ -115,7 +131,7 @@ fun Category(
                 text = item,
                 color = if (item == viewModel.selectedOption.value) MaterialTheme.colorScheme.onBackground else MaterialTheme.colorScheme.outline,
                 style = MaterialTheme.typography.headlineMedium,
-                fontWeight = FontWeight.Bold,
+//                fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .padding(8.dp)
                     .drawBehind {
@@ -149,6 +165,117 @@ fun Category(
                     ) {
                         viewModel.setSelectedOption(item)
                     }
+            )
+        }
+    }
+}
+
+@Composable
+fun GenreOption(
+    viewModel: HomeViewModel
+) {
+    val genres = if (viewModel.selectedOption.value == "Tv Shows") {
+        viewModel.tvShowsGenres.value
+    } else {
+        viewModel.moviesGenres.value
+    }
+
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(items = genres) { genre ->
+            Text(
+                text = genre.name,
+//                style = typography.body1.merge(),
+//                color = Color.White,
+                modifier = Modifier
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 8.dp,
+                        ),
+                    )
+                    .clickable {
+                        if (viewModel.selectedOption.value == "Movies") {
+                            viewModel.setGenre(genre.name)
+                            viewModel.getTrendingMovies(genre.id)
+                            viewModel.getTopRatedMovies(genre.id)
+                            viewModel.getUpComingMovies(genre.id)
+                            viewModel.getNowPlayingMovies(genre.id)
+                            viewModel.getPopularMovies(genre.id)
+                        } else if (viewModel.selectedOption.value == "Tv Shows") {
+                            viewModel.setGenre(genre.name)
+                            viewModel.getTrendingTvShows(genre.id)
+                            viewModel.getTopRatedTvShows(genre.id)
+                            viewModel.getAiringTodayTvShows(genre.id)
+                            viewModel.getOnTheAirTvShows(genre.id)
+                            viewModel.getPopularTvShows(genre.id)
+                        }
+                    }
+                    .background(
+                        if (genre.name == viewModel.selectedGenre.value) {
+                            Color.Green
+                        } else {
+                            Color.Red
+                        }
+                    )
+                    .padding(
+                        end = 8.dp,
+                    )
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GenreOptionV2(
+    viewModel: HomeViewModel
+) {
+    val genres = if (viewModel.selectedOption.value == "Tv Shows") {
+        viewModel.tvShowsGenres.value
+    } else {
+        viewModel.moviesGenres.value
+    }
+    val selected by remember {
+        mutableStateOf(true)
+    }
+
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(items = genres) { genre ->
+            FilterChip(
+                selected = if (genre.name == viewModel.selectedGenre.value) {
+                    selected
+                } else {
+                    !selected
+                },
+                onClick = {
+                    if (viewModel.selectedOption.value == "Movies") {
+                        viewModel.setGenre(genre.name)
+                        viewModel.getTrendingMovies(genre.id)
+                        viewModel.getTopRatedMovies(genre.id)
+                        viewModel.getUpComingMovies(genre.id)
+                        viewModel.getNowPlayingMovies(genre.id)
+                        viewModel.getPopularMovies(genre.id)
+                    } else if (viewModel.selectedOption.value == "Tv Shows") {
+                        viewModel.setGenre(genre.name)
+                        viewModel.getTrendingTvShows(genre.id)
+                        viewModel.getTopRatedTvShows(genre.id)
+                        viewModel.getAiringTodayTvShows(genre.id)
+                        viewModel.getOnTheAirTvShows(genre.id)
+                        viewModel.getPopularTvShows(genre.id)
+                    }
+                },
+                label = { Text(genre.name) },
+                selectedIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Done,
+                        contentDescription = "Localized Description",
+                        modifier = Modifier.size(FilterChipDefaults.IconSize)
+                    )
+                },
+                modifier = Modifier.padding(end = 5.dp)
             )
         }
     }
