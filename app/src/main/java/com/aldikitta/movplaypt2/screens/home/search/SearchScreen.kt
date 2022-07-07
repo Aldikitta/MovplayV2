ecbb99150024895316fe7d9cb605e4f3e5cc6695
Aldikitta <andi.kitta77@gmail.com>
2022-07-07 21:08:48 +0800

    SearchViewModel

---
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/search/SearchScreen.kt
@@ -0,0 +1,4 @@
+package com.aldikitta.movplaypt2.screens.home.search
+
+class SearchScreen {
+}
---
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/search/SearchViewModel.kt
@@ -0,0 +1,43 @@
+package com.aldikitta.movplaypt2.screens.home.search
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.filter
+import com.aldikitta.movplaypt2.data.repository.MultiSearchRepository
+import com.aldikitta.movplaypt2.model.Search
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val repository: MultiSearchRepository
+) : ViewModel() {
+    private val _searchTerm = mutableStateOf("")
+    val searchTerm: State<String> = _searchTerm
+
+    fun setSearchTerm(term: String) {
+        _searchTerm.value = term
+    }
+
+    private val _searchResult =
+        mutableStateOf<kotlinx.coroutines.flow.Flow<PagingData<Search>>>(emptyFlow())
+    val searchSearch: State<kotlinx.coroutines.flow.Flow<PagingData<Search>>> = _searchResult
+
+    fun searchAll(searchParam: String) {
+        viewModelScope.launch {
+            _searchResult.value = repository.multiSearch(searchParam).map { pagingData ->
+                pagingData.filter {
+                    ((it.title != null || it.originalName != null || it.originalTitle != null) &&
+                            (it.mediaType == "tv" || it.mediaType == "movie"))
+                }
+            }.cachedIn(viewModelScope)
+        }
+    }
+}
