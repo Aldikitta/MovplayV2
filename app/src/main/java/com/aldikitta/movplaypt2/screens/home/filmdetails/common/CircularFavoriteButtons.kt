79059ea627928f8e995d45ee10d969f4f20045bc
Aldikitta <andi.kitta77@gmail.com>
2022-07-08 14:42:24 +0800

    CircularFavoriteButtons

--- app/src/main/java/com/aldikitta/movplaypt2/screens/home/filmdetails/common/CircularFavoriteButtons.kt
+++ app/src/main/java/com/aldikitta/movplaypt2/screens/home/filmdetails/common/CircularFavoriteButtons.kt
@@ -1,9 +1,12 @@
 package com.aldikitta.movplaypt2.screens.home.filmdetails.common
 
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -22,16 +25,20 @@ fun CircularFavoriteButtons(
         onClick = {
             onClick(isLiked)
         }) {
-
         Icon(
             modifier = Modifier
-                .width(38.dp)
-                .height(38.dp),
-            imageVector = Icons.Filled.Favorite,
+                .padding(top = 8.dp)
+                .width(40.dp)
+                .height(40.dp),
+            imageVector = if (isLiked){
+                Icons.Filled.Favorite
+            } else{
+                Icons.Filled.FavoriteBorder
+            },
             tint = if (isLiked) {
                 Color.Red
             } else {
-                Color.LightGray
+                Color.White
             },
             contentDescription = if (isLiked) {
                 stringResource(id = R.string.unlike)
